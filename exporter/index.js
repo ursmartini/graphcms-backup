9aff30569279db7a17fc2d39ac1684cb3c5aaba0
Urs Martini <urs.martini@adac-camping.de>
2019-08-21 10:11:33 +0200

    functional

--- exporter/index.js
+++ exporter/index.js
@@ -1,82 +1,31 @@
 const fetch = require("node-fetch");
 
-class ApiClient {
-	constructor(endpoint, token) {
-		if (!endpoint) throw new Error("No endpoint specified");
-		if (!token) throw new Error("No token specified");
-
-		this.endpoint = endpoint
-		this.token = token
-	}
-
-	async export (cursor) {
-	
-		const res = await fetch(this.endpoint, {
-			method: "post",
-			body: JSON.stringify({
-				"fileType": "nodes",
-				"cursor": cursor
-			}),
-			headers: {
-				"Authorization": `Bearer ${this.token}`
-			}
-		});
-
-		if (!res.ok) { 
-			throw new Error("API error")
+const R = require('ramda');
+const apiExport = R.curry(async (endpoint,token,cursor) => {
+	const res = await fetch(endpoint, {
+		method: "post",
+		body: JSON.stringify({
+			"fileType": "nodes",
+			"cursor": cursor
+		}),
+		headers: {
+			"Authorization": `Bearer ${token}`
 		}
+	});
 
-		const resContentType = res.headers.get('content-type')
-		if (resContentType != "application/json") {
-			throw new Error(
-				`Invalid content-type: ${resContentType}` )
-		}
-		let json = await res.json()
-		return json.out.jsonElements
-	}
-}
-
-class FileWriter {
-	constructor() {}
-	async save() {}
-}
-
-class FileReader {
-	constructor() {}
-	async read() {}
-}
-
-class ImportExport {
-	constructor(apiClient, fileWriter) {
-		this.apiClient = apiClient
-		this.fileWriter = fileWriter
-
-		console.log("this.fileWriter" + fileWriter)
+	if (!res.ok) { 
+		throw new Error("API error")
 	}
 
-	async export () {
-
-		const isEof = (res) => !res.isFull
-		const nextCursor = (res) => res?res.cursor:null
-
-// 		do {
-// 			let result = await this.apiClient.export(nextCursor())
-// 
-// 			let [data, cursor, eof] = this.converter.convert(chunk)
-// 			const foo = await this.fileWriter.save(data)
-// 			console.log(foo)
-// 
-// 			const cursor = a.cursor
-// 
-// 		} while (!isEof(result));
-
-
+	const resContentType = res.headers.get('content-type')
+	if (resContentType != "application/json") {
+		throw new Error(
+			`Invalid content-type: ${resContentType}` )
 	}
-}
+	let json = await res.json()
+	return json.out.jsonElements
+})
 
 module.exports = {
-	ApiClient,
-	ImportExport,
-	FileWriter,
-	FileReader
+	apiExport
 }
--- test/single_graphcms_export.test.js
+++ test/single_graphcms_export.test.js
@@ -4,9 +4,7 @@ const chaiNock = require('chai-nock');
 
 chai.use(chaiNock);
 
-const { 
-	ApiClient, ImportExport, FileWriter, FileReader
-} = require('../exporter');
+const { apiExport } = require('../exporter');
 
 const sampleResponse = {
 	"out": {
@@ -16,7 +14,8 @@ const sampleResponse = {
 	}
 }
 
-const client = new ApiClient('http://endpoint/api', 'token')
+const myApiExport = apiExport("http://endpoint/api", "token")
+
 
 describe('Content versioning', () => {
 	beforeEach(() => {
@@ -28,23 +27,6 @@ describe('Content versioning', () => {
 
 	describe('API client', () => {
 
-		test('Should require an endpoint', () => {
-			expect(() => {
-				new ApiClient('', 'token')
-			}).toThrow("No endpoint specified")
-		});
-
-		test('Should require a token', () => {
-			expect(() => {
-				new ApiClient('endpoint', '')
-			}).toThrow("No token specified")
-		});
-
-		test('Should construct successfully', () => {
-			expect(new ApiClient('endpoint', 'token'))
-				.toBeInstanceOf(ApiClient)
-		});
-
 		test('Should request from API', async () => {
 			const scope = nock("http://endpoint")
 				.post("/api")
@@ -54,7 +36,7 @@ describe('Content versioning', () => {
 					{"content-type": "application/json"}
 				)
 
-			await expect(client.export())
+			await expect(myApiExport(null))
 				.resolves
 				.toEqual(sampleResponse.out.jsonElements)
 
@@ -66,7 +48,7 @@ describe('Content versioning', () => {
 				.post("/api")
 				.reply(500)
 
-			await expect(client.export())
+			await expect(myApiExport(null))
 				.rejects
 				.toThrow("API error");
 
@@ -85,7 +67,7 @@ describe('Content versioning', () => {
 
 			// we're not interested in the actual result or further logic
 			// but only in the passing of auth-headers
-			try { await client.export() } catch (e) {}
+			try { await myApiExport(null) } catch (e) {}
 
 			scope.done()
 		})
@@ -96,7 +78,7 @@ describe('Content versioning', () => {
 			const scope = nock("http://endpoint").post("/api")
 				.reply(200, "foobar", { "content-type": "foo/bar" })
 
-			await expect(client.export())
+			await expect(myApiExport(null))
 				.rejects
 				.toThrow("Invalid content-type: foo/bar")
 		});
@@ -115,69 +97,16 @@ describe('Content versioning', () => {
 					{ "content-type": "application/json" }
 				)
 
-			const cursor = {"foo": "bar"}
-
-			await expect(
-				client.export(cursor)
-			).resolves.toEqual(sampleResponse.out.jsonElements)
+			await expect(myApiExport({"foo": "bar"}))
+				.resolves.toEqual(sampleResponse.out.jsonElements)
 
 			scope.done()
 
 		});
 		test('Should fetch nodes', async () => {
 
-			const sampleResponse = {
-				"out": {
-					"jsonElements": "this should be returned"
-				}
-			}
-			const scope = nock("http://endpoint").post("/api")
-				.reply(200, sampleResponse, { 
-					"content-type": "application/json" 
-				})
-
-			const result = await client.export("nodes")
-			expect(result).toBeDefined()
-			expect(result).toEqual("this should be returned");
-			scope.done()
-
-		});
-
-	});
-
-	describe("FileSystem writer", () => {
-		test('Should write ...', async () => {
-		});
-	
-	});
-
-
-	describe("Exporter", () => {
-		test("Should orchestrate ", async () => {
-
-			const apiClient = {
-				export: jest.fn()
-			}
-			apiClient.export.mockResolvedValue("foobar")
-			apiClient.export.mockResolvedValue("foobar")
-
-			const converter = {
-				convert: jest.fn().mockResolvedValue("convertted")
-			}
-
-			const fileWriter = {
-				save: jest.fn().mockResolvedValue("schnarz")
-			}
-
-			const importExport = new ImportExport(apiClient, converter, fileWriter);
-			importExport.export()
-
-			console.log(fileWriter.save.mock.calls);
-
-			// expect(apiClient.export).toHaveBeenCalled()
-//			expect(fileWriter.save).toHaveBeenCalled()
-
 		});
 	});
+
 
 });
